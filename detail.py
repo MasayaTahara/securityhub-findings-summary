6fb1c961332f84895ea5b39d2b1c2e0f3bd28a5f
MasayaTahara <mocoso.0u1@gmail.com>
2022-01-07 18:55:48 +0900

    Add column of finding detail

--- detail.py
+++ detail.py
@@ -1,11 +1,15 @@
-def create_finding_str(compliance_status: str, severity_label: str, title: str, description: str, resource_ids: list, standards: list) -> dict:
+def create_finding_dict(compliance_status: str, severity: str,
+                        title: str, description: str, resource_ids: list, standards: list,
+                        finding_id: str, created_at: str) -> dict:
     finding = {
-        'ComplianceStatus': compliance_status,
-        'SeverityLabel': severity_label,
+        'FindingID': finding_id,
+        'ResourceIds': resource_ids,
         'Title': title,
         'Description': description,
-        'ResourceIds': resource_ids,
+        'Severity': severity,
+        'CreatedAt': created_at,
         'Standards': standards,
+        'ComplianceStatus': compliance_status,
     }
     return finding
 
@@ -15,6 +19,7 @@ def get_findings_detail(findings) -> list:
     for f in findings:
         compliance = f.get('Compliance')
         if compliance != None:
+            finding_id = f.get('Id')
             compliance_status = compliance.get('Status')
             severity = f.get('Severity').get('Label')
             title = f.get('Title')
@@ -22,8 +27,10 @@ def get_findings_detail(findings) -> list:
             resource_ids = [resource.get('Id')
                             for resource in f.get('Resources')]
             standards = f.get('Types')
+            created_at = f.get('CreatedAt')
 
-            finding_detail = create_finding_str(
-                compliance_status=compliance_status, severity_label=severity, title=title, description=description, resource_ids=resource_ids, standards=standards)
+            finding_detail = create_finding_dict(
+                finding_id=finding_id, compliance_status=compliance_status, severity=severity, title=title, description=description, resource_ids=resource_ids, standards=standards,
+                created_at=created_at)
             findings_detail.append(finding_detail)
     return findings_detail
