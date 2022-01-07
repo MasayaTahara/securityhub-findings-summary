ba968a83a56b16dce80dcdc618fa7846ad86b166
MasayaTahara <mocoso.0u1@gmail.com>
2022-01-08 00:41:20 +0900

    Add pytest

---
+++ tests/test_diff.py
@@ -0,0 +1,28 @@
+import src.diff
+
+import os
+import csv
+
+
+def test_get_summary():
+
+    # Case1 NEW/MODIFIED/DELETED
+
+    previous = get_findings_from_csv(
+        'tests/data/SecurityHub_findings_status_200001010000.csv')
+    current = get_findings_from_csv(
+        'tests/data/SecurityHub_findings_status_200001020000.csv')
+    summary = get_findings_from_csv(
+        'tests/data/SecurityHub_findings_status_200001030000.csv')
+
+    assert summary == src.diff.get_summary(
+        current_findings_detail=current, previous_findings_detail=previous)
+
+
+def get_findings_from_csv(file_name):
+    findings = []
+    with open(os.path.join(os.getcwd(), file_name), 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            findings.append(row)
+        return findings
