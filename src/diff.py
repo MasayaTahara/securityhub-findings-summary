51c56ab47790a9270e3555bfdb499287deed5ff0
MasayaTahara <mocoso.0u1@gmail.com>
2022-01-08 11:17:50 +0900

    Implement get_summary

--- src/diff.py
+++ src/diff.py
@@ -1,21 +1,46 @@
 def get_summary(current_findings_detail, previous_findings_detail) -> list:
-    current_findings_summary = []
-    for current_finding in current_findings_detail:
-        # for previous_finding in previous_findings_detail:
-            #     print(previous_finding)
+    findings_summary = []
 
-        current_findings_summary.append(create_summary_dict(
+    # Get previous/current finding IDs
+    previous_finding_ids = [previous_finding.get(
+        'FindingID') for previous_finding in previous_findings_detail]
+    current_finding_ids = [current_finding.get(
+        'FindingID') for current_finding in current_findings_detail]
+
+    for current_finding in current_findings_detail:
+        # Check whether previous finding IDs contain current finding ID
+        if current_finding.get('FindingID') in previous_finding_ids:
+            status = 'NOTIFIED'
+        else:
+            status = 'NEW'
+
+        findings_summary.append(create_summary_dict(
             finding_id=current_finding['FindingID'],
-            resource_ids=current_finding['ResourceIds'],
+            resource_ids=current_finding['ResourceIDs'],
             title=current_finding['Title'],
             description=current_finding['Description'],
             severity=current_finding['Severity'],
             created_at=current_finding['CreatedAt'],
             standards=current_finding['Standards'],
-            # TODO: Status check logic needed
-            status='NEW',
+            status=status,
         ))
-    return current_findings_summary
+
+    # Check whether current finding IDs contain previous finding ID (not DELETED status)
+    for previous_finding in previous_findings_detail:
+        if previous_finding.get('Status') != 'DELETED':
+            if not previous_finding.get('FindingID') in current_finding_ids:
+                findings_summary.append(create_summary_dict(
+                    finding_id=previous_finding['FindingID'],
+                    resource_ids=previous_finding['ResourceIDs'],
+                    title=previous_finding['Title'],
+                    description=previous_finding['Description'],
+                    severity=previous_finding['Severity'],
+                    created_at=previous_finding['CreatedAt'],
+                    standards=previous_finding['Standards'],
+                    status='DELETED',
+                ))
+
+    return findings_summary
 
 
 def create_summary_dict(status: str, severity: str,
--- tests/test_diff.py
+++ tests/test_diff.py
@@ -2,18 +2,56 @@ import src.diff
 
 import os
 import csv
+import pytest
 
 
-def test_get_summary():
-
-    # Case1 NEW/MODIFIED/DELETED
-
+def test_get_summary_1():
+    # Case1 1 MODIFIED
     previous = get_findings_from_csv(
         'tests/data/SecurityHub_findings_status_200001010000.csv')
     current = get_findings_from_csv(
         'tests/data/current_findings_1.csv')
     summary = get_findings_from_csv(
+        'tests/data/summary_1.csv')
+
+    assert summary == src.diff.get_summary(
+        current_findings_detail=current, previous_findings_detail=previous)
+
+
+def test_get_summary_2():
+    # Case2 2 MODIFIED
+    previous = get_findings_from_csv(
+        'tests/data/SecurityHub_findings_status_200001020000.csv')
+    current = get_findings_from_csv(
+        'tests/data/current_findings_2.csv')
+    summary = get_findings_from_csv(
+        'tests/data/summary_2.csv')
+
+    assert summary == src.diff.get_summary(
+        current_findings_detail=current, previous_findings_detail=previous)
+
+
+def test_get_summary_3():
+    # Case3 NEW/MODIFIED
+    previous = get_findings_from_csv(
         'tests/data/SecurityHub_findings_status_200001030000.csv')
+    current = get_findings_from_csv(
+        'tests/data/current_findings_3.csv')
+    summary = get_findings_from_csv(
+        'tests/data/summary_3.csv')
+
+    assert summary == src.diff.get_summary(
+        current_findings_detail=current, previous_findings_detail=previous)
+
+
+def test_get_summary_4():
+    # Case4 NEW/MODIFIED/DELETED
+    previous = get_findings_from_csv(
+        'tests/data/SecurityHub_findings_status_200001040000.csv')
+    current = get_findings_from_csv(
+        'tests/data/current_findings_4.csv')
+    summary = get_findings_from_csv(
+        'tests/data/summary_4.csv')
 
     assert summary == src.diff.get_summary(
         current_findings_detail=current, previous_findings_detail=previous)
